4d678ca8963a925b148a853d46a3f1feb73262e7
Richard Kellner <richard.kellner@gmail.com>
2017-05-18 10:07:14 +0200

    Fixed GA click tracking when content security policy was in place
    I have moved GA javascript code into external file and wrote function
    that bind on click and send event to GA. Because CSP will block all
    inline scripts.

--- html/js/ga.js
+++ html/js/ga.js
@@ -1,19 +1,31 @@
-      // generic GA
+// Load GA js code
 
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-87330218-1', 'auto');
-      ga('send', 'pageview');
+ga('create', 'UA-87330218-1', 'auto');
+ga('send', 'pageview');
 
-      var ua = navigator.userAgent || navigator.vendor || window.opera;
+var ua = navigator.userAgent || navigator.vendor || window.opera;
 
-      if ((ua.indexOf('FBAN') > -1) || (ua.indexOf('FBAV') > -1)) {
-        document.getElementById('menu').style.display = 'none';
-      }
+if ((ua.indexOf('FBAN') > -1) || (ua.indexOf('FBAV') > -1)) {
+    document.getElementById('menu').style.display = 'none';
+}
 
+// Function that send event to GA
+var trackOutboundLink = function(mouseclick) {
+    ga('send', 'event', 'Outbound Link', mouseclick.target.href);
+}
+
+// AddEventListener for links with external class and notify GA with url on click
+document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.getElementsByClassName('external');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', trackOutboundLink);
+    }
+});
       
       // YouTube player tracking
 
@@ -57,11 +69,4 @@
           }
       }
 
-      // Outbound clicks
-    
-    var trackOutboundLink = function(url) {
-        ga('send', 'event', 'outbound', 'click', url, {
-            'transport': 'beacon',
-            'hitCallback': function(){document.location = url;}
-        });
-    }
+
